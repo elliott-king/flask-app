27744f3aa6fcfcf628d620a0fc93b85b6f568a5a
Peter Valdez <peter@shipyard.build>
2020-03-24 17:33:17 -0400

    Return the headers

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 
 app = Flask(__name__)
@@ -6,4 +6,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'This is a pull request!'
+    return str(request.headers)
